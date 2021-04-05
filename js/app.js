2126db5d88c04f97d1a6cc928f44113c60a00678
Christian Leichtweis <chris99leichtweiss@gmail.com>
2021-04-05 17:23:05 -0300

    almost there

--- js/app.js
+++ js/app.js
@@ -31,17 +31,54 @@
                 item.img = `img-cart${partPath}`;
 
                 let name = 
-                event.target.parentElement.parentElement.nextElementSibling;
+                event.target.parentElement.parentElement.nextElementSibling.
+                children[0].children[0].textContent;
+                item.name = name;
+                let price = 
+                event.target.parentElement.parentElement.nextElementSibling.
+                children[0].children[1].textContent;
 
-                console.log(name);
+                let finalPrice = price.slice(1).trim();
 
-                console.log(item);
+                item.price = finalPrice;
+
+                //console.log(finalPrice);
+
+                //console.log(name);
+
+                //console.log(item);
+
+                const cartItem = document.createElement('div'); 
+
+                cartItem.classList.add(
+                    'cart-item', 
+                    'd-flex',
+                    'justify-content-between',
+                    'text-capitalize', 
+                    'my-3'
+                    );
+
+                cartItem.innerHTML = `
+                    <img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
+                        <div class="item-text">
+                            <p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p>
+                            <span>R$</span>
+                            <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
+                        </div>
+                        <a href="#" id='cart-item-remove' class="cart-item-remove">
+                        <i class="fas fa-trash"></i>
+                        </a>
+                    </div> 
+                    `;
+
+                //select cart 
+                const cart = document.getElementById('cart');
+                const total = document.querySelector('.cart-total-container');
+
+                cart.insertBefore(cartItem, total);
+                alert('item added to the cart');
 
             }
-            
-    
         });
-    
-
     });
 }) ();
